5b0ef5050856a12c351c13688ad976d99724afa6
Jonathan Cancio <jonahcancio@gmail.com>
2025-12-07 12:22:36 +0800

    Add UP college degree

--- src/portfolio.js
+++ src/portfolio.js
@@ -380,30 +380,17 @@ const competitiveSites = {
 const degrees = {
   degrees: [
     {
-      title: "Indian Institute of Information Technology Kurnool",
-      subtitle: "B.Tech. in Computer Engineering",
-      logo_path: "iiitk_logo.png",
-      alt_name: "IIITDM Kurnool",
-      duration: "2016 - 2020",
+      title: "University of the Philippines Diliman",
+      subtitle: "BS Computer Science",
+      logo_path: "up_logo.png",
+      alt_name: "CompSci",
+      duration: "2015 - 2020",
       descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ I have a solid foundation in core software engineering subjects like Data Structures, Algorithms, Database Management Systems (DBMS), Operating Systems (OS), Computer Architecture (CA), Computer Networks, Artificial Intelligence (AI), and Agile methodologies.",
+        "⚡ I have completed specialized courses in Deep Learning, Data Science, Cloud Computing, and Full-Stack Web Development.",
+        "⚡ I graduated Magna Cum Laude with a final GPA equivalent of 1.33.",
       ],
-      website_link: "http://iiitk.ac.in",
-    },
-    {
-      title: "Indiana University Bloomington",
-      subtitle: "M.S. in Computer Science",
-      logo_path: "iu_logo.png",
-      alt_name: "Indiana University Bloomington",
-      duration: "2021 - 2023",
-      descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
-      ],
-      website_link: "https://www.indiana.edu/",
+      website_link: "https://upd.edu.ph/",
     },
   ],
 };
